bc3af3c874683ba067bbe23334c7fd9624412bf4
soyoung931014 <soyoung931014@gmail.com>
2023-11-14 23:41:16 +0900

    pra

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,8 @@ const menuText = document.querySelector("#espresso-menu-name");
 
 const menuBox = document.querySelector("#espresso-menu-list");
 
+let items = [];
+
 function createMenuBox(menu) {
   const list_box = document.createElement("div");
   list_box.classList.add("menu-list-box");
@@ -22,17 +24,23 @@ function createMenuBox(menu) {
   const btn_sold_out = document.createElement("button");
   btn_sold_out.className = "bg-gray-50 text-gray-500 text-sm mr-1 sold_out";
   btn_sold_out.textContent = "품절";
+
   const btn_edit = document.createElement("button");
-  btn_edit.classList.add("edit");
+  btn_edit.className = "bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button";
   btn_edit.textContent = "수정";
+
   const btn_del = document.createElement("button");
-  btn_del.classList.add("del");
+  btn_del.className = "bg-gray-50 text-gray-500 text-sm menu-remove-button";
   btn_del.textContent = "삭제";
+
   btn_del.onclick = del;
 
   menu_btn_box.append(btn_sold_out, btn_edit, btn_del);
   list_box.appendChild(menu_btn_box);
   menuBox.appendChild(list_box);
+  let item = { id: Math.random(), name: menu, soldOut: false };
+  items.push(item);
+  localStorage.setItem("expresso", JSON.stringify(items));
 }
 
 menuBtn.addEventListener("submit", function (e) {
@@ -53,4 +61,29 @@ function menuCount() {
 
 function del() {
   console.log("삭제");
+  window.confirm("정말 삭제하시겠습니까?");
+}
+// 확인 버튼을 누른다.
+// 로컬스토리지에 들어간다.
+// 기본적으로 페칭은 로컬스토리지에서 데이터를 가져온다.
+
+const category = document.querySelectorAll(".cafe-category-name");
+for (let i = 0; i < category.length; i++) {
+  let categoryName = category[i].dataset.categoryName;
+  let categoryNameKor = category[i].textContent;
+  category[i].onclick = () => coffeeCategory(categoryName, categoryNameKor);
+}
+
+function coffeeCategory(categoryName, korName) {
+  console.log(categoryName);
+
+  const categoryText = document.querySelector(".mt-1");
+  categoryText.textContent = `${korName} 메뉴 관리`;
+  console.log(localStorage.getItem(categoryName));
+  // const menu_count = document.querySelector(".menu-count");
+  // menu_count.textContent = `총 ${
+  //   localStorage.getItem("categoryName").length
+  // }개`;
+
+  //여기에서 로컬스토리지에서 가져와 getItem
 }
